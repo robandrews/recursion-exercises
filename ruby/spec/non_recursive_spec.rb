30e0a69ca4ab1878f3ef08929578474c9fe0609b
Alex Reichert <alexreichert621@gmail.com>
2014-05-23 14:22:04 -0700

    fix rspec

--- ruby/spec/non_recursive_spec.rb
+++ ruby/spec/non_recursive_spec.rb
@@ -1,25 +1,24 @@
 require_relative '../non_recursive'
 
-# Fucking alexxxxxx
-# describe "#add_nums_to_words" do
-#   it "should add decreasing numbers after words, when given a string" do
-#     expect(add_nums_to_words("This is a test")).to eq("This4 is3 a2 test1")
-#     expect(add_nums_to_words("This is not recursion")).to eq("This4 is3 not2 recursion1")
-#   end
-#   
-#   it "should return the string itself if the string doesn't have words" do
-#     expect(add_nums_to_words("")).to eq("")
-#     expect(add_nums_to_words(" ")).to eq(" ")
-#   end
-#   
-#   it "should handle single word strings" do
-#     expect(add_nums_to_words("Test")).to eq("Test1")
-#   end
-#   
-#   it "retains leading and trailing spaces" do
-#     expect(add_nums_to_words("  testing this  ")).to eq("  testing2 this1  ")
-#   end
-# end
+describe "#add_nums_to_words" do
+  it "should add decreasing numbers after words, when given a string" do
+    expect(add_nums_to_words("This is a test")).to eq("This4 is3 a2 test1")
+    expect(add_nums_to_words("This is not recursion")).to eq("This4 is3 not2 recursion1")
+  end
+  
+  it "should return the string itself if the string doesn't have words" do
+    expect(add_nums_to_words("")).to eq("")
+    expect(add_nums_to_words(" ")).to eq(" ")
+  end
+  
+  it "should handle single word strings" do
+    expect(add_nums_to_words("Test")).to eq("Test1")
+  end
+  
+  it "retains leading and trailing spaces" do
+    expect(add_nums_to_words("  testing this  ")).to eq("  testing2 this1  ")
+  end
+end
 
 describe "#combine_sorted_lists" do
   it "should return a combined sorted array, when given two sorted arrays" do
