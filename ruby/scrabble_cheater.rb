392d5e37066e6d43a6c8cfe41fe30404f8b95075
Alex Reichert <alexreichert621@gmail.com>
2014-05-26 18:04:02 -0700

    add simple scrabble cheater

--- ruby/scrabble_cheater.rb
+++ ruby/scrabble_cheater.rb
@@ -1,3 +1,26 @@
-# write methods that take in letters and a dictionary
-# and return all possible words that can be formed, 
-# ranked by scrabble point score
+# very slow, easy, naive approach...
+# need to include letters points, and better time efficiency
+
+DICTIONARY = File.readlines("dictionary.txt").map { |word| word.chomp }
+
+def find_words(str)
+  dictionary = DICTIONARY.select { |word| word.length <= str.length }
+  lets = str.split("")
+  options = []
+  (2..lets.count).each do |n|
+    options += get_combos(lets, dictionary, n)
+  end
+  options
+end
+
+def get_combos(letters, dic, length)
+  letters.permutation(length).to_a
+      .map { |w| w.join }
+      .select { |w| dic.include?(w) }.uniq
+end
+
+letters = "llopiws"
+find_words(letters).each do |word|
+  puts word
+end
+
